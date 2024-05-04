eb5b5bd7289677879e2a6cf91c083fae9f48bb97
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-05-03 22:41:27 -0600

    mking dark mode a choice

---
+++ week2/dom-basic.js
@@ -0,0 +1,7 @@
+const fulname = 'Julio Cesar Tavarez';
+console.log(fulname);
+const h1 = document.querySelector('h1');
+h1.innerHTML = '<em>' + fulname + '</em>';
+
+const section = document.createElement('section');
+const h2 = document.createElement('h2')
