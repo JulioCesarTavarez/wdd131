a827f7c9d499f35dd136dd0a21d610c8517f502c
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-05-09 12:46:54 -0600

    fixing dark mode

--- mission/mission.js
+++ mission/mission.js
@@ -1,12 +1,13 @@
 const themeSelector = document.getElementById("bright-mode")
 function changeTheme(){
     if (themeSelector.value == "dark"){
+        console.log("Dark")
         document.body.classList.add("dark-mode");
         document.querySelector("img").src = "byui-logo_white.png"
     } else {
+        console.log("Light")
         document.body.classList.remove("dark-mode");
         document.querySelector("img").src = "byui-logo_blue.webp"
     }
-
-    themeSelector.addEventListener('change', changeTheme);
 }
+themeSelector.addEventListener('change', changeTheme);
